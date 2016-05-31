c00849742a506f1884ce8a413bed1d754d93fa84
huialfred <huialfred@outlook.com>
2016-05-31 21:36:23 +0800

    first modify

--- src/test_prj/MyTest.java
+++ src/test_prj/MyTest.java
@@ -3,7 +3,7 @@ package test_prj;
 public class MyTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("this is first git project");
 
 	}
 
